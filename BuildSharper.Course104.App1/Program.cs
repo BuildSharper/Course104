65a017b1834b1846acb08cfb7092c9278c3fd691
BuildSharper <buildsharper@gmail.com>
2022-09-07 22:24:02 -0500

    Initial commit

---
+++ BuildSharper.Course104.App1/Program.cs
@@ -0,0 +1,101 @@
+﻿Console.WriteLine("*****************************************");
+Console.WriteLine("Let's build a calculator using functions!");
+Console.WriteLine("*****************************************");
+Console.WriteLine("");
+
+//let's call the GetNumber() function to get the two numbers
+var number1 = GetNumber("first");
+var number2 = GetNumber("second");
+
+//let's create a 'done' variable for our while loop.
+var done = false;
+
+while (!done)
+{
+    //clear the screen and display the menu
+    Console.Clear();
+    Console.WriteLine("Select the equation you'd like to run:");
+    Console.WriteLine($"1.) ADDITION {number1} + {number2} = ?");
+    Console.WriteLine($"2.) SUBTRACTION {number1} - {number2} = ?");
+    Console.WriteLine($"3.) MULTIPLICATION {number1} * {number2} = ?");
+    Console.WriteLine($"4.) DIVISION {number1} / {number2} = ?");
+    Console.WriteLine("5.) Exit");
+
+    var input = Console.ReadLine();
+
+    //ensure that the user entered an integer
+    if (int.TryParse(input, out var selection))
+    {
+        //execute the appropriate function using a switch
+        switch (selection)
+        {
+            case 1:
+                Console.WriteLine($"{number1} + {number2} = {Addition(number1, number2)}");
+                break;
+            case 2:
+                Console.WriteLine($"{number1} - {number2} = {Subtraction(number1, number2)}");
+                break;
+            case 3:
+                Console.WriteLine($"{number1} * {number2} = {Multiplication(number1, number2)}");
+                break;
+            case 4:
+                Console.WriteLine($"{number1} / {number2} = {Division(number1, number2)}");
+                break;
+            case 5:
+                Console.WriteLine("Bye bye!");
+                done = true;
+                break;
+            default:
+                Console.WriteLine("Invalid selection, please try again.");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid selection, please try again.");
+    }
+
+    Console.ReadLine();
+}
+
+#region Functions
+
+static int GetNumber(string numberName)
+{
+    while (true)
+    {
+        Console.Write($"Enter the {numberName} number:");
+        var input = Console.ReadLine();
+
+        if (int.TryParse(input, out var number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("That's not a number, please try again!");
+        }
+    }
+}
+
+static int Addition(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+static int Subtraction(int number1, int number2)
+{
+    return number1 - number2;
+}
+
+static int Multiplication(int number1, int number2)
+{
+    return number1 * number2;
+}
+
+static double Division(int number1, int number2)
+{
+    return number1 / (double)number2;
+}
+
+#endregion
---
+++ BuildSharper.Course104.App2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.CompilerServices;
+
+//let's define a global variable, number, that will be used for adding
+int number = 0;
+
+//first, lets add 1 to number 10 times using By Value (separate copy of the variable)
+Console.WriteLine("By Value:");
+for (int i = 0; i < 10; i++)
+{
+    AddByVal(number);
+}
+
+//next, lets add 1 to number 10 times using By Reference (points to the existing variable)
+Console.WriteLine("By Reference:");
+for (int i = 0; i < 10; i++)
+{
+    AddByRef(ref number);
+}
+
+//finally, lets add 1 to number 10 times using a global variable (no variable is scoped locally)
+Console.WriteLine("Global Variable:");
+for (int i = 0; i < 10; i++)
+{
+    AddGlobal();
+}
+
+Console.WriteLine("Press any key to continue.");
+Console.ReadLine();
+
+#region Functions
+
+//NOTE there is no "ref" before "int number", this makes the variable By Value
+void AddByVal(int number)
+{
+    number++;
+    Console.WriteLine($"\t{number}");
+}
+
+//NOTE "ref" before "int number", this makes the variable By Reference
+void AddByRef(ref int number)
+{
+    number++;
+    Console.WriteLine($"\t{number}");
+}
+
+//NOTE there are no parameters to this function, so "number" within this function is a global variable
+void AddGlobal()
+{
+    number++;
+    Console.WriteLine($"\t{number}");
+}
+
+#endregion
